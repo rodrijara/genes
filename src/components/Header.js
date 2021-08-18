3679d29accd8b284198b16de81b00bec0f449c27
Rodrigo Jara <rodri@Rodrigos-MacBook-Pro.local>
2021-08-18 18:11:03 +0200

    add header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+export const Header = () => {
+  return (
+    <div>
+      <h1>Genes</h1>
+      <div>
+        <ul>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
